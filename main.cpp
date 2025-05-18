af120a03aa4d652d497e5a2652ab1ca8984d9d29
Brayden Parker <braydenparker2701@gmail.com>
2025-05-18 20:43:53 +1000

    circle

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@
 #include "ray.h"
 #include "vec3.h"
 
+bool hitSphere(const point3& center, double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc,oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 colour rayColour(const ray& r){
+    if (hitSphere(point3(0,0,-1), 0.5, r))
+        return colour(1, 0, 0);
+
     vec3 unitDirection = unitVector(r.direction());
     auto a = 0.5*(unitDirection.y() + 1.0);
     return (1.0-a)*colour(1.0,1.0,1.0) + a*colour(0.5,0.7,1.0);
