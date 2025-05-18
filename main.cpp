cec151d94b5d5afaff13ad2742f21094fdc7d6e5
Brayden Parker <braydenparker2701@gmail.com>
2025-05-18 18:36:26 +1000

    added viewport and camera stuff

--- main.cpp
+++ main.cpp
@@ -2,14 +2,44 @@
 #include <fstream>
 
 #include "colour.h"
+#include "ray.h"
 #include "vec3.h"
+
+colour rayColour(const ray& r){
+    vec3 unitDirection = unitVector(r.direction());
+    auto a = 0.5*(unitDirection.y() + 1.0);
+    return (1.0-a)*colour(1.0,1.0,1.0) + a*colour(0.5,0.7,1.0);
+}
 
 int main() {
 
     // Image
 
+    auto aspectRatio = 1/1;
     int imageWidth = 512;
-    int imageHeight = 512;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    int imageHeight = int(imageWidth / aspectRatio);
+    imageHeight = (imageHeight < 1) ? 1 : imageHeight;
+
+    // Camera
+
+    auto focalLength = 1.0;
+    auto viewportHeight = 2.0;
+    auto viewportWidth = viewportHeight * (double(imageWidth) / imageHeight);
+    auto cameraCenter = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewportU = vec3(viewportWidth,0,0);
+    auto viewportV = vec3(0,-viewportHeight,0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixelDeltaU = viewportU / imageWidth;
+    auto pixelDeltaV = viewportV / imageHeight;
+
+    // Calculate the location of the upper left pixel.
+    auto viewportUpperLeft = cameraCenter - vec3(0,0,focalLength) - viewportU/2 - viewportV/2;
+    auto pixel00Loc = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
 
     std::string outFileName {"image.ppm"};
 
@@ -27,7 +57,11 @@ int main() {
     for (int j = 0; j < imageHeight; j++) {
         std::clog << "\rScanlines remaining: " << (imageHeight - j) << ' ' << std::flush;
         for (int i = 0; i < imageWidth; i++) {
-            auto pixelColour = colour(double(i) / (imageWidth - 1), double(j) / (imageHeight - 1), 0.25);
+            auto pixelCentre = pixel00Loc + (i * pixelDeltaU) + (j * pixelDeltaV);
+            auto rayDirection = pixelCentre - cameraCenter;
+            ray r(cameraCenter, rayDirection);
+
+            colour pixelColour = rayColour(r);
             write_colour(outFile, pixelColour);
         }
     }
