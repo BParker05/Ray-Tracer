9e74155ee48cc5045c942988d0b736ccbcd6d0d5
Brayden Parker <braydenparker2701@gmail.com>
2025-05-31 22:53:29 +1000

    surface normals + better ray-sphere intersection

--- main.cpp
+++ main.cpp
@@ -5,18 +5,26 @@
 #include "ray.h"
 #include "vec3.h"
 
-bool hitSphere(const point3& center, double radius, const ray& r){
+double hitSphere(const point3& center, double radius, const ray& r){
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc,oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h*h - a*c;
+    
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (h - std::sqrt(discriminant)) / a;
+    }
 }
 
 colour rayColour(const ray& r){
-    if (hitSphere(point3(0,0,-1), 0.5, r))
-        return colour(1, 0, 0);
+    auto t = hitSphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unitVector(r.at(t) - vec3(0,0,-1));
+        return 0.5*colour(N.x()+1, N.y()+1, N.z()+1);
+    }
 
     vec3 unitDirection = unitVector(r.direction());
     auto a = 0.5*(unitDirection.y() + 1.0);
